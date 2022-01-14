0ad9525f500df7473fb7cb01961e7df5d71d86e5
Vlad-Vargan <blach.smith.2000@gmail.com>
2022-01-14 23:01:35 +0200

    log admins_str

--- bot/admins.py
+++ bot/admins.py
@@ -1,6 +1,9 @@
 import os
 from typing import List
 
+from loguru import logger
+
 
 admins_str: str = os.getenv("ADMINS", "")
+logger.info(f"admin ids: {admins_str}")
 ADMINS: List[int] = list(map(int, admins_str.split(" ")))
